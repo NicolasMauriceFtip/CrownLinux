17c0b72c1993a28e91b52afd1412719d4e297283
NicolasMauriceItescia <nicolas.maurice@edu.itescia.fr>
2021-03-17 19:08:23 +0000

    story 1 done

--- log_dump.sh
+++ log_dump.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [[ ! -d "$2" ]];
+then
+	mkdir .$2
+fi
 if [[ -e "$1" ]];
 then
-	var1 = $(touch log_$(date +%X)_$(date +%T).log)
-	cd $1
+	cp $1 .$2/log_$(date +%F)_$(date +%T).log
+	truncate -s 0 $1
+fi
