a33a6dce71ed49b93f3ce97c2362794caada7a4e
lea78 <lea.hamzi@edu.itescia.fr>
2021-03-18 12:46:21 +0100

    début story 4

--- log_format.sh
+++ log_format.sh
@@ -3,11 +3,7 @@
 if [[ -e "$1" ]];
 then
 	touch Log_$(date +%F)-$(date +%T).json
-	cat $1 | while read line;
-	do
-		varDate = grep $(date)
-		vartype = $( echo $line | grep "error")
-		echo {\"date":\"$varDate"','"\"type\":\"$varType\""}
-	done
+	awk -v var1= "$1"
+	awk '{print $1$2}'	
 fi
 		 
